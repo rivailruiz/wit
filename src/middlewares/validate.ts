d95ef5fe919087f07c961ec75261027778ea104f
Ruiz <alexander.rivail@gmail.com>
2022-05-09 21:17:06 +0100

    wip validate

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,25 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../models/custom-error';
 
-
 function validate(
-  err: TypeError | CustomError,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  let customError = err;
-
   const { a, b } = req.body;
-
-
-  if (!(a instanceof CustomError)) {
+  let customError;
+  if (!a) {
     customError = new CustomError(
       "The operator 'a' cannot be null."
     );
+    res.status((customError as CustomError).status).send(customError);
   }
 
-  res.status((customError as CustomError).status).send(customError);
+  if (!b) {
+    customError = new CustomError(
+      "The operator 'b' cannot be null."
+    );
+    res.status((customError as CustomError).status).send(customError);
+  }
+
+  next();
 };
 
 export default validate;
