9af99939594f8f01a73a87765c6c63f6a0e47ff2
Ruiz <alexander.rivail@gmail.com>
2022-05-09 19:14:44 +0100

    add controller

--- src/controllers/operations.ts
+++ src/controllers/operations.ts
@@ -1,7 +1,37 @@
-'use strict';
+import express, { Request, Response } from 'express';
+import { validate } from '../models/validation';
 
-let controllers = {
-    addition: async () => {
-        console.log('controller')
-    }
+const addiction = (req: Request, res: Response) => {
+    const { operative1, operative2 } = req.body;
+    res.send(`WIT Calc: ${operative1 + operative2}`);
+};
+
+const subtraction = (req: Request, res: Response) => {
+    const { operative1, operative2 } = req.body;
+    res.send(`WIT Calc: ${operative1 - operative2}`);
 }
+
+const division = (req: Request, res: Response) => {
+    const { operative1, operative2 } = req.body;
+    res.send(`WIT Calc: ${operative1 / operative2}`);
+}
+
+const multiplication = (req: Request, res: Response) => {
+    const { operative1, operative2 } = req.body;
+    res.send(`WIT Calc: ${operative1 / operative2}`);
+}
+
+
+
+const validateFields = (operative1: number, operative2: number) => {
+    if(!operative1 || isNaN(operative1)){
+        return {"sucess": false, "message": "please, verify the operative1 field."}
+    }
+    if(!operative2 || isNaN(operative2)){
+        return {"sucess": false, "message": "please, verify the operative2 field."}
+    }
+
+    return true;
+}
+
+module.exports = { addiction, subtraction, division, multiplication };
--- src/routes.ts
+++ src/routes.ts
@@ -2,41 +2,27 @@ import express, { Request, Response } from 'express';
 import { validate } from './models/validation';
 
 
+const operationsController = require('./controllers/operations');
+
+
 const router = express.Router();
 
 router.post('/api/addition', async (req: Request, res: Response) => {
-    const { operative1, operative2 } = req.body;
-
-    return res.send(`WIT Calc: ${operative1 + operative2}`);
+    await operationsController.addiction(req, res);
 })
 
 router.post('/api/subtraction', async (req: Request, res: Response) => {
-    const { operative1, operative2 } = req.body;
-
-    return res.send(`WIT Calc: ${operative1 - operative2}`);
+    await operationsController.subtraction(req, res);
 })
 
 router.post('/api/division', async (req: Request, res: Response) => {
-    const { operative1, operative2 } = req.body;
-
-    return res.send(`WIT Calc: ${operative1 / operative2}`);
+    await operationsController.division(req, res);
 })
 
-router.post('/api/multiplication', async (req: Request, res: Response) => {
-    const { operative1, operative2 } = req.body;
-
-    return res.send(`WIT Calc: ${operative1 * operative2}`);
+router.post('/api/multiplication', async (req: Request, res: Response) => { 
+    await operationsController.multiplication(req, res);
 })
 
-const validateFields = (operative1: number, operative2: number) => {
-    if(!operative1 || isNaN(operative1)){
-        return {"sucess": false, "message": "please, verify the operative1 field."}
-    }
-    if(!operative2 || isNaN(operative2)){
-        return {"sucess": false, "message": "please, verify the operative2 field."}
-    }
 
-    return true;
-}
 
 export { router as MainRouter }
